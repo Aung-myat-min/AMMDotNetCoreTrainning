4827cfda0c98b50b1f6e67399a109097f58240c6
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-09-13 17:45:40 +0630

    feat: Reading Data using Adapter

--- AMMDotNetCoreTrainningConsole/Program.cs
+++ AMMDotNetCoreTrainningConsole/Program.cs
@@ -1,5 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+using System.Data.SqlClient;
+
 Console.WriteLine("Hello, World!");
 //Console.ReadLine();
+
+string connectionString = "Data Source=DESKTOP-KPCHONN\\SQLEXPRESS;Initial Catalog=DotNetTrainning;User ID=sa;Password=sasa@123";
+Console.WriteLine("Connection String: " + connectionString);
+
+SqlConnection connection = new SqlConnection(connectionString);
+
+Console.WriteLine("Connection Opening...");
+connection.Open();
+Console.WriteLine("Connection Opened!");
+
+string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+  FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
+
+SqlCommand cmd = new SqlCommand(query, connection);
+SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+DataTable dt = new DataTable();
+
+adapter.Fill(dt);
+
+Console.WriteLine("Connection Closing...");
+connection.Close();
+Console.WriteLine("Connection Closed!");
+
+foreach (DataRow dr in dt.Rows)
+{
+    Console.WriteLine(dr["BlogId"]);
+    Console.WriteLine(dr["BlogTitle"]);
+    Console.WriteLine(dr["BlogAuthor"]);
+    Console.WriteLine(dr["BlogContent"]);
+}
 
 Console.ReadKey();

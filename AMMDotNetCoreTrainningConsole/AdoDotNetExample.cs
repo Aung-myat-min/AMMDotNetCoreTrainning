84963c3c8dcdb4fb7adb04a99aba43e8d79c2b05
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-09-15 01:58:23 +0630

    created: ado dot net example class, READ method

---
+++ AMMDotNetCoreTrainningConsole/AdoDotNetExample.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMMDotNetCoreTrainningConsole
+{
+    public class AdoDotNetExample
+    {
+
+        private readonly string connectionString = "Data Source=DESKTOP-KPCHONN\\SQLEXPRESS;Initial Catalog=DotNetTrainning;User ID=sa;Password=sasa@123";
+
+        public void Read()
+        {
+            SqlConnection connection = new SqlConnection(connectionString);
+            Console.WriteLine("Connection Opening...");
+            connection.Open();
+            Console.WriteLine("Connection Opened!");
+
+            string query = @"SELECT [BlogId]
+                  ,[BlogTitle]
+                  ,[BlogAuthor]
+                  ,[BlogContent]
+              FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            //SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            //DataTable dt = new DataTable();
+
+            //adapter.Fill(dt);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["BlogId"]);
+                Console.WriteLine(reader["BlogTitle"]);
+                Console.WriteLine(reader["BlogAuthor"]);
+                Console.WriteLine(reader["BlogContent"]);
+            }
+
+            Console.WriteLine("Connection Closing...");
+            connection.Close();
+            Console.WriteLine("Connection Closed!");
+
+            //foreach (DataRow dr in dt.Rows)
+            //{
+            //    Console.WriteLine(dr["BlogId"]);
+            //    Console.WriteLine(dr["BlogTitle"]);
+            //    Console.WriteLine(dr["BlogAuthor"]);
+            //    Console.WriteLine(dr["BlogContent"]);
+            //}
+        }
+
+
+    }
+}
--- AMMDotNetCoreTrainningConsole/Program.cs
+++ AMMDotNetCoreTrainningConsole/Program.cs
@@ -1,51 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AMMDotNetCoreTrainningConsole;
 using System.Data;
 using System.Data.SqlClient;
 
 Console.WriteLine("Hello, World!");
 //Console.ReadLine();
 
-string connectionString = "Data Source=DESKTOP-KPCHONN\\SQLEXPRESS;Initial Catalog=DotNetTrainning;User ID=sa;Password=sasa@123";
-Console.WriteLine("Connection String: " + connectionString);
-
-SqlConnection connection = new SqlConnection(connectionString);
-
-Console.WriteLine("Connection Opening...");
-connection.Open();
-Console.WriteLine("Connection Opened!");
-
-string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-  FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
-
-SqlCommand cmd = new SqlCommand(query, connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-//DataTable dt = new DataTable();
-
-//adapter.Fill(dt);
-
-SqlDataReader reader = cmd.ExecuteReader();
-
-while (reader.Read())
-{
-    Console.WriteLine(reader["BlogId"]);
-    Console.WriteLine(reader["BlogTitle"]);
-    Console.WriteLine(reader["BlogAuthor"]);
-    Console.WriteLine(reader["BlogContent"]);
-}
-
-Console.WriteLine("Connection Closing...");
-connection.Close();
-Console.WriteLine("Connection Closed!");
-
-//foreach (DataRow dr in dt.Rows)
-//{
-//    Console.WriteLine(dr["BlogId"]);
-//    Console.WriteLine(dr["BlogTitle"]);
-//    Console.WriteLine(dr["BlogAuthor"]);
-//    Console.WriteLine(dr["BlogContent"]);
-//}
+AdoDotNetExample adoDotNet = new AdoDotNetExample();
+adoDotNet.Read();
 
 Console.ReadKey();

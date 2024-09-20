861c407920beacc7d4fc31aa77990a21f47828ef
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-09-20 21:05:30 +0630

    create: dapper read

---
+++ AMMDotNetCoreTrainningConsole/DapperExample.cs
@@ -0,0 +1,37 @@
+﻿using AMMDotNetCoreTrainningConsole.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AMMDotNetCoreTrainningConsole
+{
+    internal class DapperExample
+    {
+        private readonly string _connectionString = "Data Source=DESKTOP-KPCHONN\\SQLEXPRESS;Initial Catalog=DotNetTrainning;User ID=sa;Password=sasa@123";
+
+        public void Read()
+        {
+            string query = @"SELECT [BlogId]
+                  ,[BlogTitle]
+                  ,[BlogAuthor]
+                  ,[BlogContent]
+              FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
+            using (IDbConnection db = new SqlConnection(_connectionString))
+            {
+                var data = db.Query<BlogDataModel>(query);
+                foreach(var item in data)
+                {
+                    Console.WriteLine(item.BlogId);
+                    Console.WriteLine(item.BlogTitle);
+                    Console.WriteLine(item.BlogContent);
+                    Console.WriteLine(item.BlogAuthor);
+                }
+            }
+        }
+    }
+}
--- AMMDotNetCoreTrainningConsole/Program.cs
+++ AMMDotNetCoreTrainningConsole/Program.cs
@@ -6,11 +6,14 @@ using System.Data.SqlClient;
 Console.WriteLine("Hello, World!");
 //Console.ReadLine();
 
-AdoDotNetExample adoDotNet = new AdoDotNetExample();
-//adoDotNet.Read();
-//adoDotNet.Write();
-//adoDotNet.ReadById();
-//adoDotNet.Update();
-adoDotNet.Delete();
+//AdoDotNetExample adoDotNet = new AdoDotNetExample();
+////adoDotNet.Read();
+////adoDotNet.Write();
+////adoDotNet.ReadById();
+////adoDotNet.Update();
+//adoDotNet.Delete();
+
+DapperExample dapperExample = new DapperExample();
+dapperExample.Read();
 
 Console.ReadKey();

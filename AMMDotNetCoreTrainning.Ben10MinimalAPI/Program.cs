89a1d47a9a6b6e8af15431ae0795080fd589a6b1
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-11-08 23:56:51 +0630

    feat: get alien by id

--- AMMDotNetCoreTrainning.Ben10MinimalAPI/Program.cs
+++ AMMDotNetCoreTrainning.Ben10MinimalAPI/Program.cs
@@ -2,6 +2,7 @@ using Newtonsoft.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 const string filePath = "Data/ben10.json";
+var data = File.ReadAllText(filePath);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -21,11 +22,38 @@ app.UseHttpsRedirection();
 
 app.MapGet("/ben10", () =>
 {
-    var data = File.ReadAllText(filePath);
     var result = JsonConvert.DeserializeObject<Ben10ResponseModel>(data);
     return Results.Ok(result.Tbl_Ben10);
 })
     .WithName("GetAliens")
+    .WithOpenApi();
+
+app.MapGet("/ben10/{id}", (int id) =>
+{
+    var result = JsonConvert.DeserializeObject<Ben10ResponseModel>(data);
+    if (result.Tbl_Ben10.Length == 0)
+    {
+        return Results.NotFound("The database is empty!");
+    }
+
+    Tbl_Ben10? targetAlien = null;
+    foreach (var alien in result.Tbl_Ben10)
+    {
+        if (alien.id == id)
+        {
+            targetAlien = alien;
+            break;
+        }
+    }
+
+    if (targetAlien is null)
+    {
+        return Results.NotFound("Alien not found!");
+    }
+
+    return Results.Ok(targetAlien);
+})
+    .WithName("GetAlienByName")
     .WithOpenApi();
 
 app.Run();

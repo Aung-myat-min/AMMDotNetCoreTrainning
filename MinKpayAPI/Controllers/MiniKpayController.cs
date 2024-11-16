fb32ba8c051c8be3fa84c2e8c1a47bda977c4e7a
Aung-myat-min <koaungmyatmin0@gmail.com>
2024-11-16 22:00:11 +0630

    feat: user api CRUD

---
+++ MinKpayAPI/Controllers/MiniKpayController.cs
@@ -0,0 +1,58 @@
+﻿using AMMDotNetCoreTrainning.Domain.Features.MiniKpay;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MiniKPay.Database.Models;
+using System.Text.RegularExpressions;
+
+namespace MinKpay.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MiniKpayController : ControllerBase
+    {
+        private readonly MiniKpayService _kpayService;
+        private readonly PersonService _personService;
+        private readonly HistoryService _historyService;
+
+        public MiniKpayController()
+        {
+            _kpayService = new MiniKpayService();
+            _personService = new PersonService();
+            _historyService = new HistoryService();
+        }
+
+        [HttpGet("{mobileNo}")]
+        public IActionResult GetPersonByMobile(string mobile)
+        {
+            var person = _personService.GetPersonByMobileNo(mobile);
+            if (person == null)
+            {
+                return NotFound("Person Not Found!");
+            }
+            return Ok(person);
+        }
+
+        [HttpPost()]
+        public IActionResult CreateNewAccount(TblPerson person)
+        {
+            string mobilePattern = @"^(09|01)[0-9]{6,9}$";
+            string pinPattern = @"^[0-9]{6}$";
+            if (person == null)
+            {
+                return BadRequest("Person can't be empty!");
+            }
+            if (String.IsNullOrEmpty(person.FullName) || person.FullName.Length < 4)
+            {
+                return BadRequest("Full name should be 4 characters minimum.");
+            }
+            if (String.IsNullOrEmpty(person.MobileNo) || !Regex.IsMatch(person.MobileNo, mobilePattern))
+            {
+                return BadRequest("Mobile Number should be \n- 8 digits mimum\n- 11 digits maximum\n- And, should starts from 01 or 09");
+            }
+            if (String.IsNullOrEmpty(person.Pin) || !Regex.IsMatch(person.Pin, pinPattern))
+            {
+                return BadRequest("A Pin should be a combination of 6 digits");
+            }
+        }
+    }
+}
---
+++ MinKpayAPI/Controllers/PersonController.cs
@@ -0,0 +1,130 @@
+﻿using AMMDotNetCoreTrainning.Domain.Features.MiniKpay;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MiniKPay.Database.Models;
+using System;
+using System.Text.RegularExpressions;
+
+namespace MinKpay.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly PersonService _personService;
+        private readonly MiniKpayService _kpayService;
+        private readonly AppDbContext _db;          //for development purpose
+        private readonly string mobilePattern = @"^(09|01)[0-9]{6,9}$";
+        private readonly string pinPattern = @"^[0-9]{6}$";
+
+        public PersonController()
+        {
+            _personService = new PersonService();
+            _kpayService = new MiniKpayService();
+            _db = new AppDbContext();
+        }
+
+        //For Development Purpose
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var list = _db.TblPeople.AsNoTracking().ToList();
+            return Ok(list);
+        }
+
+        [HttpGet("{mobileNo}")]
+        public IActionResult GetPersonByMobile(string mobile)
+        {
+            var person = _personService.GetPersonByMobileNo(mobile);
+            if (person == null)
+            {
+                return NotFound("Person Not Found!");
+            }
+            return Ok(person);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNewAccount(TblPerson person)
+        {
+
+            if (person == null)
+            {
+                return BadRequest("Person can't be empty!");
+            }
+            if (String.IsNullOrEmpty(person.FullName) || person.FullName.Length < 4)
+            {
+                return BadRequest("Full name should be 4 characters minimum.");
+            }
+            if (String.IsNullOrEmpty(person.MobileNo) || !Regex.IsMatch(person.MobileNo, mobilePattern))
+            {
+                return BadRequest("Mobile Number should be \n- 8 digits mimum\n- 11 digits maximum\n- And, should starts from 01 or 09");
+            }
+            if (String.IsNullOrEmpty(person.Pin) || !Regex.IsMatch(person.Pin, pinPattern))
+            {
+                return BadRequest("A Pin should be a combination of 6 digits");
+            }
+
+            var newUser = _personService.CreateAccount(person);
+            if (newUser == null)
+            {
+                return BadRequest("Person Creation Failed!");
+            }
+
+            return Ok(newUser);
+        }
+
+        [HttpPatch("{mobileNo}/{pin}")]
+        public IActionResult UpdateDetails(string mobileNo, string pin, TblPerson person)
+        {
+            if (person == null)
+            {
+                return BadRequest("Person can't be empty!");
+            }
+            if (String.IsNullOrEmpty(person.FullName) || person.FullName.Length < 4)
+            {
+                return BadRequest("Full name should be 4 characters minimum.");
+            }
+            if (String.IsNullOrEmpty(person.MobileNo) || !Regex.IsMatch(person.MobileNo, mobilePattern))
+            {
+                return BadRequest("Mobile Number should be \n- 8 digits mimum\n- 11 digits maximum\n- And, should starts from 01 or 09");
+            }
+            if (String.IsNullOrEmpty(person.Pin) || !Regex.IsMatch(person.Pin, pinPattern))
+            {
+                return BadRequest("A Pin should be a combination of 6 digits");
+            }
+
+            bool? result = _kpayService.CheckPin(mobileNo, pin);
+            if (result is null || result == false)
+            {
+                return BadRequest("Invalid mobile no or pin code!");
+            }
+
+            var updatedUser = _personService.UpdatePerson(mobileNo, person);
+            if (updatedUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(updatedUser);
+        }
+
+        [HttpDelete("{mobileNo}/{pin}")]
+        public IActionResult Delete(string mobileNo, string pin)
+        {
+            bool? result = _kpayService.CheckPin(mobileNo, pin);
+            if (result is null || result == false)
+            {
+                return BadRequest("Invalid mobile no or pin code!");
+            }
+
+            var updatedUser = _personService.DeletePerson(mobileNo);
+            if (updatedUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok("User Deleted!");
+        }
+    }
+}

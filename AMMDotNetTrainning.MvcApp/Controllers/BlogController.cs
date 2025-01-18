8b245241f6b5bff045eec95f3ddfbaa2b4d03344
Aung Myat Min <koaungmyatmin0@gmail.com>
2025-01-18 13:30:41 +0630

    feat: mvc app create

---
+++ AMMDotNetTrainning.MvcApp/Controllers/BlogController.cs
@@ -0,0 +1,21 @@
+﻿using AMMDotNetCoreTrainning.Domain.Features.Blog;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMMDotNetTrainning.MvcApp.Controllers
+{
+    public class BlogController : Controller
+    {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
+        public IActionResult Index()
+        {
+            var lst = _blogService.GetTblblogs();
+            return View(lst);
+        }
+    }
+}

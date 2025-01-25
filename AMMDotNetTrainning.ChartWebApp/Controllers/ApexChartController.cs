e5bb3edf1ddb08c5583f09199952ac0e81579180
Aung Myat Min <koaungmyatmin0@gmail.com>
2025-01-26 02:56:22 +0630

    feat: apexchart column chart

---
+++ AMMDotNetTrainning.ChartWebApp/Controllers/ApexChartController.cs
@@ -0,0 +1,21 @@
+﻿using AMMDotNetTrainning.ChartWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMMDotNetTrainning.ChartWebApp.Controllers
+{
+    public class ApexChartController : Controller
+    {
+        public IActionResult ColumnChart()
+        {
+            var _chartData = new ColumnChartModel();
+            _chartData.Title = "Monthly Inflation in Argentina, 2002";
+            _chartData.Categories = new List<string> { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+            _chartData.Data = new List<ColumnChartData>
+            {
+                new ColumnChartData("Inflation",
+                new List<double>{ 2.3, 3.1, 4.0, 10.1, 4.0, 3.6, 3.2, 2.3, 1.4, 0.8, 0.5, 0.2 })
+            };
+            return View("ColumnChart", _chartData);
+        }
+    }
+}
---
+++ AMMDotNetTrainning.ChartWebApp/Models/ColumnChartModel.cs
@@ -0,0 +1,21 @@
+﻿namespace AMMDotNetTrainning.ChartWebApp.Models
+{
+    public class ColumnChartModel
+    {
+        public string Title { get; set; }
+        public List<string> Categories { get; set; }
+        public List<ColumnChartData> Data { get; set; }
+    }
+
+    public class ColumnChartData
+    {
+        public string name { get; set; }
+        public List<double> data { get; set; }
+
+        public ColumnChartData(string n, List<double> d) 
+        {
+            this.name = n;
+            this.data = d;
+        }
+    }
+}

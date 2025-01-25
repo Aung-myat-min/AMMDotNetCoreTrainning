e68dbb2a3bda44c29953f2e4416b15cd04ee237a
Aung Myat Min <koaungmyatmin0@gmail.com>
2025-01-25 23:48:08 +0630

    feat: chartjs bar chart

---
+++ AMMDotNetTrainning.ChartWebApp/Controllers/ChartJsController.cs
@@ -0,0 +1,21 @@
+﻿using AMMDotNetTrainning.ChartWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMMDotNetTrainning.ChartWebApp.Controllers
+{
+    public class ChartJsController : Controller
+    {
+        public IActionResult BarChart()
+        {
+            var data = new BarChartModel();
+            data.Title = "Chart.js Bar Chart with Rounded Corners";
+            data.XRowNames = new List<string> { "January", "February", "March", "April", "May", "June", "July" };
+            data.Data = new List<BarChartData>
+            {
+                    new BarChartData("Fully Rounded", new List<int>{ 30, 50, 70, 60, 90, 42, 78 }),
+                    new BarChartData("Small Radius", new List<int>{ 40, 20, 80, 55, 60, 30, 95 })
+            };
+            return View("BarChart", data);
+        }
+    }
+}
---
+++ AMMDotNetTrainning.ChartWebApp/Models/BarChartModel.cs
@@ -0,0 +1,20 @@
+﻿namespace AMMDotNetTrainning.ChartWebApp.Models
+{
+    public class BarChartModel
+    {
+        public string Title { get; set; }
+        public List<BarChartData> Data { get; set; }
+        public List<string> XRowNames { get; set; }
+    }
+    public class BarChartData
+    {
+        public string Title { get; set; }
+        public List<int> Data { get; set; }
+
+        public BarChartData(string title, List<int> data)
+        {
+            this.Title = title;
+            this.Data = data;
+        }
+    }
+}

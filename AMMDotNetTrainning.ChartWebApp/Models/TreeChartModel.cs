876371900ea44b283f2f0b9b2d420bb213a74c0e
Aung Myat Min <koaungmyatmin0@gmail.com>
2025-01-26 12:07:06 +0630

    feat: highchart tree chart

---
+++ AMMDotNetTrainning.ChartWebApp/Models/TreeChartModel.cs
@@ -0,0 +1,8 @@
+﻿namespace AMMDotNetTrainning.ChartWebApp.Models
+{
+    public class TreeChartModel
+    {
+        public string Title { get; set; }
+        public List<List<object>> Data { get; set; }
+    }
+}

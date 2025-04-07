8452207f9ca57455b968b9ca1c49e0d54e339e06
Aung Myat Min <koaungmyatmin0@gmail.com>
2025-04-08 01:33:45 +0630

    feat: just a commit

--- AMMDotNetTrainning.Console2/Program.cs
+++ AMMDotNetTrainning.Console2/Program.cs
@@ -1,9 +1,13 @@
 ﻿using AMMDotNetCoreTrainning.Database.Models;
+using Microsoft.EntityFrameworkCore;
 
-EfCoreDbContext db = new EfCoreDbContext();
-var list = db.TblBlogs.Where(x => x.DeleteFlag == false).ToList();
+//EfCoreDbContext db = new EfCoreDbContext(opt =>
+//{
+//    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
+//});
+//var list = db.TblBlogs.Where(x => x.DeleteFlag == false).ToList();
 
-foreach (var item in list)
-{
-    Console.Write($"Blog Id: {item.BlogId}\nBlog Title: {item.BlogTitle}\nBlog Content: {item.BlogContent}\nWritten By: {item.BlogAuthor}\n\n");
-}
+//foreach (var item in list)
+//{
+//    Console.Write($"Blog Id: {item.BlogId}\nBlog Title: {item.BlogTitle}\nBlog Content: {item.BlogContent}\nWritten By: {item.BlogAuthor}\n\n");
+//}
